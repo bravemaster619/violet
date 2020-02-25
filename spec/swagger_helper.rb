098efead6a2ec2ece07d8de66dff9343e37dba56
bravemaster619 <bravemaster619@hotmail.com>
2020-02-25 12:08:48 +0800

    changed api endpoint from yaml to json

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -13,13 +13,49 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
-    'v1/swagger.yaml' => {
+    'v1/swagger.json' => {
       openapi: '3.0.1',
       info: {
-        title: 'API V1',
+        title: 'Violet API V1',
         version: 'v1'
       },
-      paths: {}
+      basePath: '/api/v1',
+      securityDefinitions: {
+          basic: {
+              type: :basic
+          },
+      },
+      definitions: {
+          object_id: {
+              type: :object,
+              properties: {
+                  "$oid": {type: :string}
+              }
+          },
+          filtered_user: {
+              type: :object,
+              properties: {
+                  _id: { '$ref': '#/definitions/object_id' },
+                  name: {type: :string},
+                  email: {type: :string},
+                  birthday: {type: :string, format: :date},
+                  verified: {type: :boolean},
+                  created_at: {type: :string, format: "date-time"},
+                  updated_at: {type: :string, format: "date-time"},
+                  deleted_at: {type: :string, format: "date-time"}
+              },
+              required: ['name', 'email', 'created_at', 'updated_at']
+          },
+          user: {
+              all_of: { '$ref': '#definitions/filtered_user'},
+              type: :object,
+              properties: {
+                  password_digest: { type: :string },
+                  email_code: { type: :string },
+                  allowed: { type: :boolean }
+              }
+          }
+      }
     }
   }
 
@@ -27,5 +63,5 @@ RSpec.configure do |config|
   # The swagger_docs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.swagger_format = :yaml
+  config.swagger_format = :json
 end
